b15ec58a65fbe479c7d7d3e839733d3ca82c5ed7
Stefan <27005@ikbenanna.nl>
2020-07-21 14:38:12 +0200

    cleaned up code with comments

--- semiBasic.py
+++ semiBasic.py
@@ -1,13 +1,17 @@
 import basic
 import os
 
-def compare(path1, path2):
+#full path example: C:\users\<username>\desktop\testdir\dir\file.txt
+#base path example: C:\users\<username>\desktop\testdir\
+#relative path example: dir\file.txt
+
+def compare(path1, path2):#requires full paths
     if basic.read(path1) == basic.read(path2):
         return True
     else:
         return False
 
-def index(dir):
+def index(dir):#requires base path, returns full paths
     output = []
     for x in os.listdir(dir):
         if os.path.isdir(dir + '\\' + x):
@@ -16,7 +20,7 @@ def index(dir):
             output.append(dir + '\\' + x)
     return output
 
-def dirIndex(dir):
+def dirIndex(dir):#requires base path, returns full paths
     output = []
     for x in os.listdir(dir):
         if os.path.isdir(dir + '\\' + x):
@@ -30,3 +34,6 @@ def convertToRelative(basePath, fullPath):
         return output
     else:
         raise RuntimeError('fullPath does not start with basePath')
+
+def decideAction(originalRelativePath, originalBasePath, targetRelativePath, targetBasePath):#returns string 'overwrite', 'remove' or 'keep'
+    
